abb4ee331c334ac491047819f28788df9e38bbe2
gluttony-10 <52977964+gluttony-10@users.noreply.github.com>
2023-10-10 22:05:38 +0800

    Add files via upload

---
+++ start.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+source /etc/network_turbo
+cd /root/stable-diffusion-webui/ && ./webui.sh -f --port 6006 --api --no-download-sd-model --xformers --enable-insecure-extension-access
