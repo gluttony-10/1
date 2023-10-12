7aaab2f692332d8f8b23d018db8e4b2fb1704214
gluttony-10 <52977964+gluttony-10@users.noreply.github.com>
2023-10-12 14:21:51 +0800

    Update start-autodl.sh

--- start-autodl.sh
+++ start-autodl.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-wget -q -O /root/start.sh https://github.com/gluttony-10/1/blob/main/start-autodl.sh
 source /etc/network_turbo
+wget -q -O /root/start.sh https://github.com/gluttony-10/1/blob/main/start-autodl.sh
 cd /root/stable-diffusion-webui/ && ./webui.sh -f --port 6006 --api --no-download-sd-model --xformers --enable-insecure-extension-access --controlnet-dir /root/autodl-fs/models/ControlNet --ckpt-dir /root/autodl-tmp/models/Stable-diffusion --embeddings-dir /root/autodl-tmp/models/embeddings --lora-dir /root/autodl-tmp/models/lora --vae-dir /root/autodl-tmp/models/vae --lyco-dir /root/autodl-tmp/models/lycoris
