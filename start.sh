abc3d4a6081f722a09299389bef1badf4fef4c0e
gluttony-10 <52977964+gluttony-10@users.noreply.github.com>
2023-10-12 16:17:17 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 source /etc/network_turbo
-cd /root/stable-diffusion-webui && python launch.py --port 6006 --api --no-download-sd-model --xformers --enable-insecure-extension-access --controlnet-dir /root/autodl-fs/models/ControlNet --ckpt-dir /root/autodl-tmp/models/Stable-diffusion --embeddings-dir /root/autodl-tmp/models/embeddings --lora-dir /root/autodl-tmp/models/lora --vae-dir /root/autodl-tmp/models/vae --lyco-dir /root/autodl-tmp/models/lycoris
+cd /root/stable-diffusion-webui && python launch.py --port 6006 --api --no-download-sd-model --opt-sdp-attention --enable-insecure-extension-access --controlnet-dir /root/autodl-fs/models/ControlNet --ckpt-dir /root/autodl-tmp/models/Stable-diffusion --embeddings-dir /root/autodl-tmp/models/embeddings --lora-dir /root/autodl-tmp/models/lora --vae-dir /root/autodl-tmp/models/vae --lyco-dir /root/autodl-tmp/models/lycoris
