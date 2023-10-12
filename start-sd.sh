e0d50352bd77a9339f0b813833bfea908755ce04
gluttony-10 <52977964+gluttony-10@users.noreply.github.com>
2023-10-12 16:08:14 +0800

    Update start-sd.sh

--- start-sd.sh
+++ start-sd.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 source /etc/network_turbo
-aria2c -x 16 -s 16 -k 1M -d /root https://github.com/gluttony-10/1/blob/main/start-sd.sh
+aria2c  --console-log-level=error -q -c -x 16 -s 16 -k 1M -d /root https://github.com/gluttony-10/1/blob/main/start-sd.sh
 cd /root/stable-diffusion-webui && python launch.py --port 6006 --api --no-download-sd-model --xformers --enable-insecure-extension-access --controlnet-dir /root/autodl-fs/models/ControlNet --ckpt-dir /root/autodl-tmp/models/Stable-diffusion --embeddings-dir /root/autodl-tmp/models/embeddings --lora-dir /root/autodl-tmp/models/lora --vae-dir /root/autodl-tmp/models/vae --lyco-dir /root/autodl-tmp/models/lycoris
