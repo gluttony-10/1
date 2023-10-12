0e7cfa9d2b77d93143a6fe674aca0736b26db8c3
gluttony-10 <52977964+gluttony-10@users.noreply.github.com>
2023-10-12 13:42:28 +0800

    Update and rename start.sh to start-autodl.sh

--- start-autodl.sh
+++ start-autodl.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-wget -q -O /root/start.sh https://github.com/gluttony-10/1/blob/main/start.sh
+wget -q -O /root/start.sh https://github.com/gluttony-10/1/blob/main/start-autodl.sh
 source /etc/network_turbo
 cd /root/stable-diffusion-webui/ && ./webui.sh -f --port 6006 --api --no-download-sd-model --xformers --enable-insecure-extension-access --controlnet-dir /root/autodl-fs/models/ControlNet
